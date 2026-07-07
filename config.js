0dcd3b9e863b84adaafd6c97589b94ff903dfd88
aass9122007-dotcom <aass9122007@gmail.com>
2026-07-07 06:36:26 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 // إعدادات الاتصال بـ Supabase
 // ضع بيانات مشروعك هنا (Settings > API في لوحة تحكم Supabase)
 // ============================================================
-const SUPABASE_URL = "https://ccvaaqwkmqrlhcjekzdd.supabase.co"; // مثال: https://xxxxxxxx.supabase.co
-const SUPABASE_ANON_KEY = "sb_publishable_ouo-mVf_OZHXiOL_Sk70Hg_eROn4wkV";
+const SUPABASE_URL = "https://wjvvnxjlcabzjqflidof.supabase.co"; // مثال: https://xxxxxxxx.supabase.co
+const SUPABASE_ANON_KEY = "sb_publishable_OZJ3F3qyi6dbUWKK2pH7qQ_beL19XiR";
 
 // إنشاء عميل Supabase (المكتبة محمّلة عبر CDN في index.html باسم supabase)
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
